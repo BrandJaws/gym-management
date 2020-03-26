75ae251815b190dc3dd044b8722bb7218186ab73
Zuhair47 <zuhair@brandjaws.com>
2020-03-26 19:25:18 +0500

    set image in admin header

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -14,10 +14,12 @@
                          viewBox="0 0 24 24" version="1.1" class="kt-svg-icon">
                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
                             <rect x="0" y="0" width="24" height="24"/>
-                            <path d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z"
-                                  fill="#000000" fill-rule="nonzero" opacity="0.3"/>
-                            <path d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z"
-                                  fill="#000000" fill-rule="nonzero"/>
+                            <path
+                                d="M14.2928932,16.7071068 C13.9023689,16.3165825 13.9023689,15.6834175 14.2928932,15.2928932 C14.6834175,14.9023689 15.3165825,14.9023689 15.7071068,15.2928932 L19.7071068,19.2928932 C20.0976311,19.6834175 20.0976311,20.3165825 19.7071068,20.7071068 C19.3165825,21.0976311 18.6834175,21.0976311 18.2928932,20.7071068 L14.2928932,16.7071068 Z"
+                                fill="#000000" fill-rule="nonzero" opacity="0.3"/>
+                            <path
+                                d="M11,16 C13.7614237,16 16,13.7614237 16,11 C16,8.23857625 13.7614237,6 11,6 C8.23857625,6 6,8.23857625 6,11 C6,13.7614237 8.23857625,16 11,16 Z M11,18 C7.13400675,18 4,14.8659932 4,11 C4,7.13400675 7.13400675,4 11,4 C14.8659932,4 18,7.13400675 18,11 C18,14.8659932 14.8659932,18 11,18 Z"
+                                fill="#000000" fill-rule="nonzero"/>
                         </g>
                     </svg>
                 </span>
@@ -46,21 +48,36 @@
             <div class="kt-header__topbar-wrapper" data-toggle="dropdown" data-offset="0px,0px">
                 <div class="kt-header__topbar-user">
                     <span class="kt-header__topbar-welcome kt-hidden-mobile">Hi,</span>
-                    <span class="kt-header__topbar-username kt-hidden-mobile">{{ Auth::guard('admin')->user()->name }}</span>
+                    <span
+                        class="kt-header__topbar-username kt-hidden-mobile">{{ Auth::guard('admin')->user()->name }}</span>
                     <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
-                    <span class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">
-                        <img src="{{asset('assets/media/users/avatar.png')}}" alt="image">
+                    <span
+                        class="kt-badge kt-badge--username kt-badge--unified-success kt-badge--lg kt-badge--rounded kt-badge--bold">
+
+                          @if(Auth::guard('admin')->user()->userImage != "")
+                            <img src="{{asset(Auth::guard('admin')->user()->userImage->path)}}" alt="image">
+                        @endif
+                        @if(Auth::guard('admin')->user()->userImage == "")
+                            <img src="{{asset('assets/media/users/avatar.png')}}" alt="image">
+                        @endif
                     </span>
                 </div>
             </div>
-            <div class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
+            <div
+                class="dropdown-menu dropdown-menu-fit dropdown-menu-right dropdown-menu-anim dropdown-menu-top-unround dropdown-menu-xl">
                 <!--begin: Head -->
-                <div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x" style="background-image: url({{asset('assets/media/users/300_25.jpg')}})}})">
+                <div class="kt-user-card kt-user-card--skin-dark kt-notification-item-padding-x"
+                     style="background-image: url({{asset('assets/media/users/300_25.jpg')}})}})">
                     <div class="kt-user-card__avatar">
-                        <img class="kt-hidden" alt="Pic" src="{{asset('assets/media/users/300_25.jpg')}}" />
+                        <img class="kt-hidden" alt="Pic" src="{{asset('assets/media/users/300_25.jpg')}}"/>
                         <!--use below badge element instead the user avatar to display username's first letter(remove kt-hidden class to display it) -->
                         <span class="kt-badge kt-badge--lg kt-badge--rounded kt-badge--bold kt-font-success">
-                            <img src="{{asset('assets/media/users/avatar.png')}}" alt="image">
+                              @if(Auth::guard('admin')->user()->userImage != "")
+                                <img src="{{asset(Auth::guard('admin')->user()->userImage->path)}}" alt="image">
+                            @endif
+                            @if(Auth::guard('admin')->user()->userImage == "")
+                                <img src="{{asset('assets/media/users/avatar.png')}}" alt="image">
+                            @endif
                         </span>
                     </div>
                     <div class="kt-user-card__name">
@@ -87,7 +104,8 @@
                         </div>
                     </a>
                     <div class="kt-notification__custom kt-space-between">
-                        <a href="#" onclick="event.preventDefault();document.querySelector('#admin-logout-form').submit();"
+                        <a href="#"
+                           onclick="event.preventDefault();document.querySelector('#admin-logout-form').submit();"
                            class="btn btn-label btn-label-brand btn-sm btn-bold">Sign Out</a>
                         <form id="admin-logout-form" action="{{ route('admin.logout') }}" method="POST"
                               style="display: none;">
