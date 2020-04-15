fabae697ef9b0b2cd946eab5c4a816ee3c33b420
Mohsin Ikram <mohsinikram1999@gmail.com>
2020-04-15 15:53:23 +0500

    Module Icons has been changed.

--- database/seeds/GymModulesSeeder.php
+++ database/seeds/GymModulesSeeder.php
@@ -14,16 +14,16 @@ class GymModulesSeeder extends Seeder
     {
         GymModule::truncate();
         $modules = array(
-            array('name' => 'Membership', 'route' => 'membership.list', 'activeRoute' => 'gym/membership', 'icon' => 'flaticon2-group'),
+            array('name' => 'Membership', 'route' => 'membership.list', 'activeRoute' => 'gym/membership', 'icon' => 'flaticon-network'),
             array('name' => 'Employee', 'route' => 'employee.list', 'activeRoute' => 'gym/employee', 'icon' => 'flaticon-avatar'),
             array('name' => 'Members', 'route' => 'member.dashboard', 'activeRoute' => 'gym/member', 'icon' => 'flaticon-avatar'),
-            array('name' => 'Trainers', 'route' => 'trainer.list', 'activeRoute' => 'gym/trainer', 'icon' => 'fa-chalkboard-teacher'),
-            array('name' => 'Training', 'route' => 'training.list', 'activeRoute' => 'gym/training', 'icon' => 'flaticon-avatar'),
-            array('name' => 'Suppliers', 'route' => 'supplier.list', 'activeRoute' => 'gym/supplier', 'icon' => 'flaticon-avatar'),
-            array('name' => 'Treasuries', 'route' => 'treasury.list', 'activeRoute' => 'gym/treasury', 'icon' => 'flaticon2-avatar'),
-            array('name' => 'Services', 'route' => 'service.list', 'activeRoute' => 'gym/service', 'icon' => 'flaticon2-avatar'),
-            array('name' => 'Profile', 'route' => 'gym.profile', 'activeRoute' => 'admin/auth', 'icon' => 'flaticon-profile-1'),
-            array('name' => 'Shop', 'route' => 'shop.list', 'activeRoute' => 'gym/shop', 'icon' => 'fa-dungeon'),
+            array('name' => 'Trainers', 'route' => 'trainer.list', 'activeRoute' => 'gym/trainer', 'icon' => 'flaticon-presentation'),
+            array('name' => 'Training', 'route' => 'training.list', 'activeRoute' => 'gym/training', 'icon' => 'flaticon-presentation-1'),
+            array('name' => 'Suppliers', 'route' => 'supplier.list', 'activeRoute' => 'gym/supplier', 'icon' => 'flaticon-user-add'),
+            array('name' => 'Treasuries', 'route' => 'treasury.list', 'activeRoute' => 'gym/treasury', 'icon' => 'flaticon-notes'),
+            array('name' => 'Services', 'route' => 'service.list', 'activeRoute' => 'gym/service', 'icon' => 'flaticon-businesswoman'),
+            array('name' => 'Profile', 'route' => 'gym.profile', 'activeRoute' => 'admin/auth', 'icon' => 'flaticon-user'),
+            array('name' => 'Shop', 'route' => 'shop.list', 'activeRoute' => 'gym/shop', 'icon' => 'flaticon2-shopping-cart-1'),
         );
         GymModule::insert($modules);
     }
